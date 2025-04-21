6e93d29b4051f4040db8c748df793470c0337ce0
Jayani Srinivasan <srinivasan.jayani@gmail.com>
2025-04-21 07:29:35 -0700

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import StatsScreen from './screens/StatsScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Logs" component={HomeScreen} />
+        <Tab.Screen name="Stats" component={StatsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
